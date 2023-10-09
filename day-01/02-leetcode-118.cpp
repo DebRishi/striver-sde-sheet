3e2059e7ad1e745a6383581ff6424c1facbc8bb5
Debaditya Mallick <mallick.debaditya@gmail.com>
2023-10-09 21:33:21 +0530

    Initial commit

---
+++ day-01/02-leetcode-118.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+// LEECODE 118 - Pascal's Triangle
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans(numRows);
+        for (int i = 0 ; i < numRows ; i++) {
+            ans[i] = vector<int>(i+1, 1);
+            for (int j = 1 ; j < i ; j++)
+                ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+        }
+        return ans;
+    }
+};
